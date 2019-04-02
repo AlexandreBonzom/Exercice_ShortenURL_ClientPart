9d8f88e215a05594866e3556315ec241df122578
Alexandre Bonzom <bonzom.alexandre@gmail.com>
2019-04-02 18:31:50 +0200

    trying to add router

--- src/App.js
+++ src/App.js
@@ -2,99 +2,19 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import React, { Component } from "react";
 
 import "./App.css";
-import axios from "axios";
-//import components
-import Header from "./Components/Header";
-import ArrayUrl from "./Components/ArrayUrl";
+
+import Home from "./Containers/Home";
 
 class App extends Component {
-  state = {
-    arrayShortenUrl: [],
-    normalUrl: "",
-    isAlreadyShorten: false,
-    isUrlValid: true
-  };
-
-  //handle error after a try/catch
-  errorHandler = error => {
-    if (error.response && error.response.status === 400) {
-      if (
-        error.response.data.message === "This address has been already shorten"
-      ) {
-        this.setState({ isAlreadyShorten: true, normalUrl: "" }, () => {
-          setTimeout(() => this.setState({ isAlreadyShorten: false }), 3000);
-        });
-      } else {
-        console.error({ message: error.response.data.message });
-      }
-    } else {
-      console.error({ message: error.message });
-    }
-  };
-
-  updateArrayUrl = async () => {
-    try {
-      const response = await axios.get("http://localhost:3000/read");
-      this.setState({ arrayShortenUrl: response.data, normalUrl: "" });
-    } catch (error) {
-      this.errorHandler(error);
-    }
-  };
-  componentDidMount = async () => {
-    this.updateArrayUrl();
-  };
-
-  handleChange = event => {
-    this.setState({ normalUrl: event.target.value });
-  };
-
-  handleSubmit = async e => {
-    e.preventDefault();
-    if (this.state.isUrlValid && this.state.normalUrl) {
-      try {
-        await axios.post("http://localhost:3000/create", {
-          normalUrl: this.state.normalUrl
-        });
-        this.updateArrayUrl();
-      } catch (error) {
-        this.errorHandler(error);
-      }
-    }
-  };
-
-  handleClickShorthenUrl = async shortenUrl => {
-    try {
-      await axios.post("http://localhost:3000/update", {
-        shortenUrl: shortenUrl
-      });
-      this.updateArrayUrl();
-    } catch (error) {
-      this.errorHandler(error);
-    }
-  };
-
-  handleOnBlur = () => {
-    const valid = /^(ftp|http|https):\/\/[^ "]+$/.test(this.state.normalUrl);
-
-    this.setState({ isUrlValid: valid });
-  };
-
   render() {
     return (
       <div className="App">
-        <Header
-          handleSubmit={this.handleSubmit}
-          handleChange={this.handleChange}
-          value={this.state.normalUrl}
-          isUrlValid={this.state.isUrlValid}
-          isAlreadyShorten={this.state.isAlreadyShorten}
-          handleOnBlur={this.handleOnBlur}
-        />
-
-        <ArrayUrl
-          arrayShortenUrl={this.state.arrayShortenUrl}
-          handleClickShorthenUrl={this.handleClickShorthenUrl}
-        />
+        <Router>
+          <Switch>
+            <Route path="/" exact render={props => <Home {...props} />} />
+            {/* <Route path="/:id" component={ShortenUrl} /> */}
+          </Switch>
+        </Router>
       </div>
     );
   }
---
+++ src/Containers/Home.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import axios from "axios";
+//import components
+import Header from "../Components/Header";
+import ArrayUrl from "../Components/ArrayUrl";
+
+class Home extends Component {
+  state = {
+    arrayShortenUrl: [],
+    normalUrl: "",
+    isAlreadyShorten: false,
+    isUrlValid: true
+  };
+
+  //handle error after a try/catch
+  errorHandler = error => {
+    if (error.response && error.response.status === 400) {
+      if (
+        error.response.data.message === "This address has been already shorten"
+      ) {
+        this.setState({ isAlreadyShorten: true, normalUrl: "" }, () => {
+          setTimeout(() => this.setState({ isAlreadyShorten: false }), 3000);
+        });
+      } else {
+        console.error({ message: error.response.data.message });
+      }
+    } else {
+      console.error({ message: error.message });
+    }
+  };
+
+  updateArrayUrl = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/read");
+      this.setState({ arrayShortenUrl: response.data, normalUrl: "" });
+    } catch (error) {
+      this.errorHandler(error);
+    }
+  };
+  componentDidMount = async () => {
+    this.updateArrayUrl();
+  };
+
+  handleChange = event => {
+    this.setState({ normalUrl: event.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    if (this.state.isUrlValid && this.state.normalUrl) {
+      try {
+        await axios.post("http://localhost:3000/create", {
+          normalUrl: this.state.normalUrl
+        });
+        this.updateArrayUrl();
+      } catch (error) {
+        this.errorHandler(error);
+      }
+    }
+  };
+
+  handleClickShorthenUrl = async shortenUrl => {
+    try {
+      await axios.post("http://localhost:3000/update", {
+        shortenUrl: shortenUrl
+      });
+      this.updateArrayUrl();
+    } catch (error) {
+      this.errorHandler(error);
+    }
+  };
+
+  handleOnBlur = () => {
+    const valid = /^(ftp|http|https):\/\/[^ "]+$/.test(this.state.normalUrl);
+
+    this.setState({ isUrlValid: valid });
+  };
+
+  render() {
+    return (
+      <>
+        <Header
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          value={this.state.normalUrl}
+          isUrlValid={this.state.isUrlValid}
+          isAlreadyShorten={this.state.isAlreadyShorten}
+          handleOnBlur={this.handleOnBlur}
+        />
+
+        <ArrayUrl
+          arrayShortenUrl={this.state.arrayShortenUrl}
+          handleClickShorthenUrl={this.handleClickShorthenUrl}
+        />
+      </>
+    );
+  }
+}
+
+export default Home;
